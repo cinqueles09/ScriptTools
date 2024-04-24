7b06fcda78ad05a8a32563511d10954e03ac8d28
Ismael Morilla Orellana <114949611+cinqueles09@users.noreply.github.com>
2024-04-24 21:34:10 +0200

    Update Remitentes_Seguros.ps1

--- Remitentes_Seguros.ps1
+++ Remitentes_Seguros.ps1
@@ -1,3 +1,9 @@
+#SYNOPSIS: Añadir remitentes seguros
+#DESCRIPTION: 
+#Añade remitentes seguros a todos los usuarios de la compañía
+#VERSION: 1.0
+#DATE: 06/04/2024
+
 $buzones = Get-Mailbox -ResultSize Unlimited
 $trustedSenderDomain = "[Remitente Seguro]"
 foreach ($buzon in $buzones)
